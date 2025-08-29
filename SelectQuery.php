06b2c0b8806b23729b878a206bf2c2a8a176c8cb
Moises Araujo dos Santos <moisesaraujodossantos10@gmail.com>
2025-08-29 15:49:12 -0300

    arrumando o metodo where

--- SelectQuery.php
+++ SelectQuery.php
@@ -1,54 +1,23 @@
 <?php
 
-class SelectQuery
+class SelectQuery extends Query
 {
-    public array $queries = [
-        'select' => '',
-        'from'   => '',
-        'where'  => '',
-        'order'  => '',
-        'limit'  => '',
-    ];
+
 
     public function select(array $columns)
     {
         $this->queries["select"] = 'SELECT ' . implode(",", $columns) . " ";
         return $this;
     }
-    public function from(string $table, ?string $alias = null)
-    {
-        if (isset($alias)) {
-            $this->queries["from"] = ' FROM ' . $table . ' as ' . $alias . " ";
-            return $this;
-        }
-        $this->queries["from"] = ' FROM ' . $table . " ";
-        return $this;
-    }
-    private function genericOrdemClause(int $Quantity, array $fieldExpression, ?array $modifiers = NULL)
-    {
-        $formated = [];
-        $formated[] = $fieldExpression[0];
-        if ($Quantity != 1 and count($fieldExpression) == $Quantity and count($modifiers) == $Quantity - 1) {
 
-            for ($i = 1; $i < count($fieldExpression); $i++) {
-                if ($i < count($fieldExpression)) {
-                    $formated[] = $modifiers[($i - 1) % count($modifiers)];
-                    $formated[] = $fieldExpression[$i];
-                }
-            }
-        }
-        return $formated;
-    }
+
     //o numbercondition vai ser opcional na mae, mas aqui obrigatorio, é so colocar um if e lançar exceção
-    public function where(int $NumberCondiction, array $condiction, ?array $operator = null)
+    public function where(?int $NumberCondiction = null, array $condiction, ?array $operator = null)
     {
-        $formatedWhere = $this->genericOrdemClause($NumberCondiction, $condiction, $operator);
-        if ($NumberCondiction != 1 and count($condiction) == $NumberCondiction and count($operator) == $NumberCondiction - 1) {
-            $this->queries["where"] =  'WHERE ' . implode(" ", $formatedWhere) . " ";
-            return $this;
+        if ($NumberCondiction == null) {
+            throw new Exception('Tem que indicar o numero de condições!!');
         }
-        $this->queries["where"] = 'WHERE ' . implode(" ", $condiction);
-        return $this;
+        return parent::where($NumberCondiction, $condiction, $operator);
     }
     public function order(int $NumbeOrder, array $columnName, ?array $order = NULL)
     {
