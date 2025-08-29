40c2fa027ce48f0fc93e5382c3ae69a63d3d1ce8
Moises Araujo dos Santos <moisesaraujodossantos10@gmail.com>
2025-08-29 15:51:55 -0300

    arrumando a chamada dos metodos da classe mae Query no Delete

--- DeleteQuery.php
+++ DeleteQuery.php
@@ -1,10 +1,10 @@
 <?php
 
-class DeleteQuery extends Query {
+class DeleteQuery extends Query{
 
 
     public function delete($tableName,$condition)
     {
-       return $this->from($tableName)->where($condition); 
+       $this->queries['delete'] = 'DELETE' . $this->from($tableName)->where(null,$condition); 
     }
 }
