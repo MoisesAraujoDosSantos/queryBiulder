ea5a5564ffd9ccc201c2b35bff9f75971ba5cc5d
Moises Araujo dos Santos <moisesaraujodossantos10@gmail.com>
2025-08-29 21:18:00 -0300

    protegendo o update contra sql injection

--- queryBiulderPrimary/UpdateQuery.php
+++ queryBiulderPrimary/UpdateQuery.php
@@ -5,20 +5,23 @@ namespace Ipeweb\QueryBiulder;
 class UpdateQuery extends Query{
 
 public function update($tableName,$condition)
-{
-    $this->queries['update'] = 'UPDATE' . $tableName;
+{   $this->validateIdentifier($tableName);
+    $this->queries['update'] = 'UPDATE ' . $tableName;
 }
 
 public function set(array $columns, array $values)
 {
     $querie = [];
+    $querieFormat = [];
     if(count($columns) != count($values)){
         throw new \Exception('colunas e valores devem ter o mesmo tamanho');
     }
     for ($i=0; $i < count($columns); $i++) { 
         $querie[] = $columns[$i] ." = ". $values[$i]; 
     }
-    $this->queries['set'] = 'SET '. implode($querie);
+    $querieFormat = $this->placeHolder($querie,"set");
+    $this->queries['set'] = 'SET '. implode($querieFormat);
+    return $this;
 }
     public function toSql()
     {
