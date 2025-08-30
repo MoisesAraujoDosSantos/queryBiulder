8473633e945a73a3eda837231cd55b51fa3c3132
Moises Araujo dos Santos <moisesaraujodossantos10@gmail.com>
2025-08-29 21:27:39 -0300

    colocando segurança contra sql injection no insert

--- queryBiulderPrimary/InsertQuery.php
+++ queryBiulderPrimary/InsertQuery.php
@@ -6,19 +6,19 @@ class InsertQuery extends Query
 {
 
     public function insert(string $tableName, array $columns) {
-        $this->queries['insert'] = 'INSERT INTO '.$tableName. ' (' .implode(",",$columns).')';
+        $this->validateIdentifier($tableName);
+        $this->validateIdentifier($columns);
+        $filtered = $this->placeHolder($columns, "insert");
+        $this->queries['insert'] = 'INSERT INTO '.$tableName. ' (' .implode(",",$filtered).')';
         return $this;
     }
 
     public function values(array|string $values)
     {
+        $valuesFiltered = $this->placeHolder($values, "values");
 
-        if (is_array($values)) {
-            $this->queries['values'] = '(' . implode(',', $values) . ')';
-        }
-        if (is_string($values)) {
-            $this->queries['values'] = '(' . $values . ')';
-        }
+        $this->queries['values'] = '(' . implode(',', $valuesFiltered) . ')';
+        
         return $this;
     }
         public function toSql()
