72bb0dde6ea10a7efbde71a866ed47404b7b6e06
Moises Araujo dos Santos <moisesaraujodossantos10@gmail.com>
2025-08-29 21:56:05 -0300

    adicionando segurança contra slq injection no order

--- queryBiulderPrimary/SelectQuery.php
+++ queryBiulderPrimary/SelectQuery.php
@@ -9,6 +9,7 @@ class SelectQuery extends Query
 
     public function select(array $columns)
     {
+        $this->validateIdentifier($columns);
         $this->queries["select"] = 'SELECT ' . implode(",", $columns) . " ";
         return $this;
     }
@@ -24,16 +25,28 @@ class SelectQuery extends Query
     }
     public function order(int $NumbeOrder, array $columnName, ?array $order = NULL)
     {
-        $formatedOrder = $this->genericOrdemClause( $columnName,$NumbeOrder, $order);
-        if ($NumbeOrder != 1 and count($columnName) == $NumbeOrder and count($order) == $NumbeOrder - 1) {
+        $this->validateIdentifier($columnName);
+        $formatedOrder = $this->genericOrdemClause( $columnName,$NumberOrder, $order);
+        if ($order !== null) {
+            foreach ($order as $typeOrder) {
+                if ($typeOrder !== "ASC" && $typeOrder !== "DESC") {
+                    throw new \InvalidArgumentException("Invalid order: $typeOrder");
+                }
+            }
+        }
+
+        if ($NumberOrder != 1 and count($columnName) == $NumberOrder and count($order) == $NumberOrder - 1) {
             $this->queries["order"] = 'ORDER BY ' . implode($formatedOrder) . " ";
             return $this;
         }
         $this->queries["order"] = 'ORDER BY ' . implode(" ", $columnName) . " ";
         return $this;
     }
-    public function limit($limitNumber)
+    public function limit(int $limitNumber)
     {
+        if ($limitNumber <= 0) {
+            throw new \InvalidArgumentException("Limit must be a positive integer.");
+        }
         $this->queries["limit"] = 'LIMIT ' . $limitNumber . " ";
         return $this;
     }
