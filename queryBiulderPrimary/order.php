240ad161ea62e06d41e4fae20fc493c23767e8b0
Moises Araujo dos Santos <moisesaraujodossantos10@gmail.com>
2025-09-01 16:27:13 -0300

    arquivo com o where arrumado

---
+++ queryBiulderPrimary/order.php
@@ -0,0 +1,63 @@
+<?php
+class order
+{
+    public $bindings;
+    public $i = 0;
+    public function order(array $conditions)
+    {
+        foreach ($conditions as $column => $value) {
+            $ordem[] = $column . " " . $value;
+        };
+        $query = implode(" ,", $ordem);
+
+        return $query;
+    }
+    public function where(array $clauses, array|string $operator, array $operation) //adicionar validação pra valor nulo se tiver mais de um operador
+    {
+        $where = [];
+        foreach ($clauses as $clause => $quantity) {
+            if (is_string($operator)) {
+                $operator = explode(" ", $operator);
+            }
+            if (is_array($operator)) {
+
+                foreach ($operator as $op) {
+                    $where = array_merge(
+                        $where,
+                        $this->biulderPlaceholder($clause, $quantity, $op, 'where')
+                    );
+                }
+            }
+            $this->i++;
+        };
+        $tratedWhere = '';
+        for ($i = 0; $i < count($where); $i++) {
+
+            if ($i == 0) {
+                $tratedWhere .= " {$where[$i]}";
+            } else {
+                $tratedWhere .= " {$operation[$i - 1]} {$where[$i]} ";
+            }
+        }
+    }
+
+    public function biulderPlaceholder(string $clause, string $quantity, string|array $operator, string $clauseName)
+    {
+        $formatedQuery = [];
+        if (is_string($operator)) {
+            $operator = explode(' ', $operator);
+        }
+        $field = trim($clause);
+        $value = trim($quantity);
+        $fieldIncrement = "{$field}_{$this->i}";
+        // evitando sobrescrita caso o nome  do campo seja o mesmo
+        $this->bindings[$clauseName][$fieldIncrement] = $value;
+        
+        $formatedQuery[] = $field .  $operator .' :' . "{$field}_{$this->i}";
+        return $formatedQuery;
+    }
+}
+
+$ordem = new order();
+$ordem->order(['idade' => 'DESC', 'nome' => 'ASC']);
+print_r($ordem->where(['idade' => '1', 'nome' => 'moises','id'=> 1], ['='], ['and','or']));
