e779066de2634e7678d100839301684e1e9fb047
Moises Araujo dos Santos <moisesaraujodossantos10@gmail.com>
2025-09-04 11:03:18 -0300

    adicionando as queries a pasta app

---
+++ queryBiulderPrimary/app/InsertQuery.php
@@ -0,0 +1,39 @@
+<?php
+namespace Ipeweb\QueryBiulder;
+
+class InsertQuery extends Query
+{
+    public array $columns = [];
+    public function insert(string $tableName, array $columns) {
+        $this->validateIdentifier($tableName);
+        $this->validateIdentifier($columns);
+        $filtered = [];
+        foreach ($columns as $col) {
+            $filtered[] = trim($col);
+        }
+        $this->queries['insert'] = 'INSERT INTO ' . $tableName . ' (' . implode(",", $filtered) . ')';
+        $this->columns = $filtered;
+        return $this;
+    }
+
+    public function values(array $values)
+    {
+        $placeholders = [];
+        foreach ($this->columns as $i => $col) {
+            $val = $values[$i] ?? null;
+            $this->bindings['values'][$col] = $val;
+            $placeholders[] = ':' . $col;
+        }
+        $this->queries['values'] = ' VALUES (' . implode(',', $placeholders) . ')';
+        return $this;
+    }
+
+    public function toSql()
+    {
+        $sqlQuery = $this->queries['insert'] . $this->queries['values'];
+        $this->querieReset();
+        return $sqlQuery . ";";
+    }
+} 
+
+
