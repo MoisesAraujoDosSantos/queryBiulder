bc403f377a6ae718770ad19811012170236e182b
Moises Araujo dos Santos <moisesaraujodossantos10@gmail.com>
2025-09-04 10:56:08 -0300

    testando as classes apos odificação

--- queryBiulderPrimary/teste.php
+++ queryBiulderPrimary/teste.php
@@ -3,35 +3,38 @@ namespace Ipeweb\QueryBiulder;
 
 use PDO;
 
-require 'vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 $pdo = new PDO("pgsql:host=localhost;port=5432;dbname= 'alura_pdo'",'postgres','@postgres');
-
+try {
+  // passou
 // $d = new DeleteQuery();
-// $d->delete('clients', ['id = 5']);
+// $d->delete('clients', ['id'=>'10'],['=']);
 // print_r($d->execute($pdo));
 // echo PHP_EOL;
 
 // passou
-$string = ' or 1=1; --';
+// $string = ' or 1=1; --';
 // $i = new InsertQuery();
 // $i->insert('clients', ['nome'])->values(['ana']);
 // $i->execute($pdo);
 // echo PHP_EOL;
 
 // passou
-// $u = new UpdateQuery();
-// $u->update('clients', ['id = 6'])
-//   ->set(['nome'], ["Jeanne D'arc"]);
-// $u->execute($pdo);
-// echo PHP_EOL;
-
-
-$s = new SelectQuery();
-$s->select(['id', 'nome'])
-  ->from('clients', 'c')
-  ->where(['nome' => 'Jeanne,Artoria'],['=']['and'])
-  ->order(['nome'=>'DESC']);
-$result = $s->execute($pdo)->fetchAll(PDO::FETCH_ASSOC);
-print_r($result);
+$u = new UpdateQuery();
+$u->update('clients')
+  ->set(['nome' => "Artoria Pendragon"],['id'=>'7'],['=']);
+print_r($u->execute($pdo));
 echo PHP_EOL;
 
+// passou
+// $s = new SelectQuery();
+// $s->select(['id', 'nome'])
+//   ->from('clients', 'c')
+//   ->where(['nome'=>'maria,Jeanne'],['='],['or'])
+//   ->order(['nome'=>'DESC']);
+// $result = $s->execute($pdo)->fetchAll(PDO::FETCH_ASSOC);
+// print_r($result);
+// echo PHP_EOL;
+} catch(\Exception $e){
+  print_r("ERRO: {$e->getMessage()}");
+}
